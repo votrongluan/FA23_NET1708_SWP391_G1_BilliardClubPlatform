021d2cb04df70ba02874ed1e43e83fdcb9b2820f
Chph21 <damchanphong@gmail.com>
2023-10-09 16:51:03 +0700

    Update project backend template

---
+++ BBP_Backend/src/test/java/com/example/BBP_Backend/BbpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BBP_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbpBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

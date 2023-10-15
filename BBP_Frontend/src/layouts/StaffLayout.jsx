9acd4a6275bd8adbf0c347908337e62b70498c67
votrongluan <trongluan115@gmail.com>
2023-10-15 20:49:03 +0700

    init staff layout and staff side bar

---
+++ BBP_Frontend/src/layouts/StaffLayout.jsx
@@ -0,0 +1,29 @@
+import {Grid, GridItem} from "@chakra-ui/react"
+import {Outlet} from "react-router-dom"
+import Sidebar from "../pages/staff/StaffSideBar.jsx";
+
+export default function RootLayout() {
+    return (
+        <Grid templateColumns="repeat(6, 1fr)" bg="gray.50">
+            {/* sidebar */}
+            <GridItem
+                as="aside"
+                colSpan={{base: 6, lg: 2, xl: 1}}
+                bg="purple.400"
+                minHeight={{lg: '100vh'}}
+                p={{base: '20px', lg: '30px'}}
+            >
+                <Sidebar/>
+            </GridItem>
+
+            {/* main content & navbar */}
+            <GridItem
+                as="main"
+                colSpan={{base: 6, lg: 4, xl: 5}}
+                p="40px"
+            >
+                <Outlet/>
+            </GridItem>
+        </Grid>
+    )
+}

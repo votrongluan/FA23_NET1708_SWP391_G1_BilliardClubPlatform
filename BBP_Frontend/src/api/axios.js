d56d4dd6e2dfbd112b571a95b44159c7a6a64670
votrongluan <trongluan115@gmail.com>
2023-10-15 10:46:43 +0700

    add base url

--- BBP_Frontend/src/api/axios.js
+++ BBP_Frontend/src/api/axios.js
@@ -1,5 +1,7 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-export default axios.create({
-    baseURL: 'http://localhost:3500'
-});
+export const baseURL = 'http://localhost:3500';
+// export default axios.create({
+//     baseURL: 'http://localhost:3500'
+// });
+

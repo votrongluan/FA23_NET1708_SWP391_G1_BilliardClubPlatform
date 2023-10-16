da25b03847fd3fa8744554417351c7c291f2480c
votrongluan <trongluan115@gmail.com>
2023-10-16 20:07:48 +0700

    add more feature to ClubBook.jsx

--- BBP_Frontend/src/pages/user/ClubBook.jsx
+++ BBP_Frontend/src/pages/user/ClubBook.jsx
@@ -10,7 +10,9 @@ import {
     Grid,
     GridItem,
     Heading,
+    HStack,
     Select,
+    Text,
     useToast,
 } from "@chakra-ui/react";
 import {CalendarIcon} from "@chakra-ui/icons";
@@ -18,6 +20,12 @@ import {useLoaderData, useParams} from "react-router-dom";
 import {GlobalContext} from "../../context/GlobalContext.jsx";
 
 function ClubBook() {
+    const allowedHour = (function getCurrentAndCeilNextHour() {
+        const now = new Date();
+        let currentHour = now.getHours();
+        return currentHour + 1;
+    })()
+
     // table
     const tables = [
         {
@@ -72,8 +80,6 @@ function ClubBook() {
         }
     ];
 
-    const [selectedTable, setSelectedTable] = useState(null);
-
     const toast = useToast();
 
     const today = new Date();
@@ -97,16 +103,36 @@ function ClubBook() {
     const hours = Array.from({length: 12}, (_, i) => 9 + i);
 
     const [selectedHours, setSelectedHours] = useState([]);
+    const [allowedTables, setAllowedTables] = useState([])
+    const [selectedTable, setSelectedTable] = useState(null);
+    const [selectedTableType, setSelectedTableType] = useState(1);
+    const [selectedDate, setSelectedDate] = useState(formatDate(today));
+
+    // Function to change allowed tables and selected table
+    const changeAllowedTable = (hours) => {
+        setSelectedTable(null);
+        if (hours.length === 0) {
+            setAllowedTables([])
+        } else {
+            setAllowedTables(tables.filter((table) => {
+                return Math.random() < 0.5;
+            }))
+        }
+    };
 
     // Function to handle hour selection
     const handleHourChange = (hour) => {
         // Check if the hour is already selected
         if ((selectedHours.includes(hour) && !selectedHours.includes(hour - 1) && !selectedHours.includes(hour + 1)) || (selectedHours.includes(hour) && !selectedHours.includes(hour - 1)) || (selectedHours.includes(hour) && !selectedHours.includes(hour + 1))) {
-            setSelectedHours(selectedHours.filter((h) => h !== hour));
+            const tmpHours = selectedHours.filter((h) => h !== hour)
+            setSelectedHours(tmpHours);
+            changeAllowedTable(tmpHours);
         } else {
             // Check if the selected hour maintains continuity
             if (areHoursContinuous([...selectedHours, hour])) {
-                setSelectedHours([...selectedHours, hour]);
+                const tmpHours = [...selectedHours, hour]
+                setSelectedHours(tmpHours);
+                changeAllowedTable(tmpHours);
             } else {
                 // Show an error or alert message here
                 toast({
@@ -153,7 +179,7 @@ function ClubBook() {
 
                 <FormControl isRequired mb="20px">
                     <FormLabel>2. Chọn loại bàn</FormLabel>
-                    <Select bgColor="white" name="tableType">
+                    <Select onChange={(e) => setSelectedTableType(e.target.value)} bgColor="white" name="tableType">
                         <option value="1">Phăng</option>
                         <option value="2">Lỗ</option>
                     </Select>
@@ -161,8 +187,11 @@ function ClubBook() {
 
                 <FormControl isRequired mb="20px">
                     <FormLabel>3. Chọn ngày</FormLabel>
-                    <Select bgColor="white" name="date">
-                        <option value={formatDate(today)}>Hôm nay, {formatDate(today)}</option>
+                    <Select bgColor="white" name="date" onChange={(e) => {
+                        setSelectedDate(e.target.value)
+                    }}>
+                        <option onClick={(e) => setSelectedDate(e.target.value)} value={formatDate(today)}>Hôm
+                            nay, {formatDate(today)}</option>
                         <option value={formatDate(tomorrow)}>
                             Ngày mai, {formatDate(tomorrow)}
                         </option>
@@ -181,12 +210,14 @@ function ClubBook() {
                         {hours.map((hour) => (
                             <GridItem key={hour}>
                                 <Button
+                                    isDisabled={selectedDate === formatDate(new Date()) && hour < allowedHour}
                                     size="lg" // You can adjust the size here
                                     variant={selectedHours.includes(hour) ? "solid" : "outline"}
                                     onClick={() => handleHourChange(hour)}
                                     colorScheme="yellow"
                                     color="black"
                                     minW="200px"
+                                    userSelect=""
                                 >
                                     {hour}h
                                 </Button>
@@ -199,22 +230,32 @@ function ClubBook() {
                     <FormLabel>5. Chọn bàn trống</FormLabel>
                     <FormHelperText mb={5}>Nếu không hiện ra bàn trống nào, hãy chọn khung giờ khác bạn
                         nhé</FormHelperText>
-                    <Flex gap={5} alignItems="center" justifyContent="center" margin="0 auto">
-                        {tables.map((table) => (
+                    <Flex gap={5} alignItems="center" justifyContent="center" margin="0 auto" wrap="wrap">
+                        {allowedTables.map((table) => (
                             <Box
                                 key={table.id}
-                                size="lg" // You can adjust the size here
-                                variant={selectedTable?.id === table.id ? "solid" : "outline"}
-                                onClick={() => selectedTable(table)}
-                                colorScheme="yellow"
-                                color="black"
-                                minW="200px"
+                                size="lg"
+                                onClick={() => {
+                                    setSelectedTable(table)
+                                }}
+                                minW="100px"
+                                bgColor={selectedTable && selectedTable.id === table.id ? "yellow.400" : "white"}
+                                textAlign="center"
+                                p={4}
+                                cursor="pointer"
+                                borderRadius="4px"
                             >
+                                <Text color="gray.500">Mã bàn:</Text>
                                 {table.id}
                             </Box>
                         ))}
                     </Flex>
                 </FormControl>
+
+                <HStack mb={10} alignItems="center" display="flex">
+                    <FormLabel margin="0">6. Thành tiền:</FormLabel>
+                    <Text color="red.500">69,000 đồng</Text>
+                </HStack>
 
                 <Button width="100%" mt={2} colorScheme="yellow" leftIcon={<CalendarIcon/>}>
                     Đặt bàn

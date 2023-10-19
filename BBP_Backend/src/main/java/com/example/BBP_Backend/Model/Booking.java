344ae43e296775b6a00174575ee679ff4138f0d8
huydq911 <chimsedihia@gmail.com>
2023-10-19 13:54:50 +0700

    update

--- BBP_Backend/src/main/java/com/example/BBP_Backend/Model/Booking.java
+++ BBP_Backend/src/main/java/com/example/BBP_Backend/Model/Booking.java
@@ -1,4 +1,36 @@
 package com.example.BBP_Backend.Model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.sql.Date;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "Booking")
 public class Booking {
+
+    @Id
+    @GeneratedValue
+    private int bookingId;
+
+    private int customerId;
+
+    @ManyToOne
+    @JoinColumn(name = "club_id")
+    @EqualsAndHashCode.Exclude
+    @ToString.Exclude
+    private Club clubId;
+
+    private int clubStaffId;
+
+    private int bookingStatusId;
+
+    private Date bookDate;
+
+    @OneToOne(mappedBy = "bookingId", cascade = CascadeType.ALL)
+    private Review review;
 }

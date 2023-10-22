cd00845212f298e9fd079cdcd046a3f7290d05e2
PomMort <hiuhaha789@gmail.com>
2023-10-22 09:44:26 +0700

    refactor: change district fake json to backend api

--- BBP_Frontend/src/api/axios.js
+++ BBP_Frontend/src/api/axios.js
@@ -1,7 +1,8 @@
-// import axios from 'axios';
+import axios from 'axios';
 
-export const baseURL = 'http://localhost:3500';
-// export default axios.create({
-//     baseURL: 'http://localhost:3500'
-// });
+export const baseURL = 'http://localhost:8080/api';
+
+export default axios.create({
+    baseURL: 'http://localhost:8080/api'
+});
 
--- BBP_Frontend/src/main.jsx
+++ BBP_Frontend/src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from "./App.jsx";
+import TestComponent from "./TestComponent.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App/>
+        <TestComponent/>
     </React.StrictMode>,
 )

7448635288357daab558f89262b0eabf06518605
PomMort <hiuhaha789@gmail.com>
2023-10-23 08:32:33 +0700

    refactor: change api link to slot, table type

--- BBP_Frontend/src/context/GlobalContext.jsx
+++ BBP_Frontend/src/context/GlobalContext.jsx
@@ -1,5 +1,5 @@
 import {createContext, useEffect, useState} from 'react';
-import {baseURL} from "../api/axios.js";
+import axios from "../api/axios.js";
 
 const GlobalContext = createContext();
 
@@ -9,92 +9,66 @@ const GlobalProvider = ({children}) => {
     const [slotMap, setSlotMap] = useState([]);
     const [bookingStatusMap, setBookingStatusMap] = useState([]);
 
-    // useEffect(() => {
-    //     axios.get('/v1/allDistrict')
-    //         .then(response => {
-    //             const data = response.data.data;
-    //             const districtMap = {};
-    //             data.forEach((district) => {
-    //                     districtMap[district.districtId] = district.districtName;
-    //                 }
-    //             );
-    //             setDistrictMap(districtMap);
-    //         })
-    //         .catch(error => {
-    //             console.error('Error fetching data:', error);
-    //         });
-    //
-    //     axios.get('/v1/allTableType')
-    //         .then(response => {
-    //             const data = response.data.data;
-    //             const tableTypeMap = {};
-    //             data.forEach((tableType) => {
-    //                     tableTypeMap[tableType.tableTypeId] = tableType.typeName;
-    //                 }
-    //             );
-    //             setTableTypeMap(tableTypeMap);
-    //         })
-    //         .catch(error => {
-    //             console.error('Error fetching data:', error);
-    //         });
-    //
-    //     axios.get('/bookingStatus/getAll')
-    //         .then(response => {
-    //             const data = response.data.data;
-    //             const bookingStatusMap = {};
-    //             data.forEach((bookingStatus) => {
-    //                     bookingStatusMap[bookingStatus.bookingStatusId] = bookingStatus.status;
-    //                 }
-    //             );
-    //             setBookingStatusMap(bookingStatusMap);
-    //         })
-    //         .catch(error => {
-    //             console.error('Error fetching data:', error);
-    //         });
-    // }, []);
     useEffect(() => {
-        // Fetch the data from the API endpoint
-        fetch(baseURL + '/district')
-            .then((response) => response.json())
-            .then((data) => {
+        axios.get('/v1/allDistrict')
+            .then(response => {
+                const data = response.data.data;
                 const districtMap = {};
                 data.forEach((district) => {
-                        districtMap[district.id] = district.name;
+                        districtMap[district.districtId] = district.districtName;
                     }
                 );
                 setDistrictMap(districtMap);
             })
-            .catch((error) => console.error('Error fetching data:', error));
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
 
-        // Fetch the data from the API endpoint
-        fetch(baseURL + '/tableType')
-            .then((response) => response.json())
-            .then((data) => {
+        axios.get('/v1/allTableType')
+            .then(response => {
+                const data = response.data.data;
                 const tableTypeMap = {};
                 data.forEach((tableType) => {
-                        tableTypeMap[tableType.id] = tableType.name;
+                        tableTypeMap[tableType.tableTypeId] = tableType.typeName;
                     }
                 );
                 setTableTypeMap(tableTypeMap);
             })
-            .catch((error) => console.error('Error fetching data:', error));
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
 
-        // Fetch the data from the API endpoint
-        fetch(baseURL + '/slot')
-            .then((response) => response.json())
-            .then((data) => {
+        axios.get('/bookingStatus/getAll')
+            .then(response => {
+                const data = response.data.data;
+                const bookingStatusMap = {};
+                data.forEach((bookingStatus) => {
+                        bookingStatusMap[bookingStatus.bookingStatusId] = bookingStatus.status;
+                    }
+                );
+                setBookingStatusMap(bookingStatusMap);
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
+
+        axios.get('/v1/getSlots')
+            .then(response => {
+                const data = response.data;
                 const slotMap = {};
                 data.forEach((slot) => {
-                        slotMap[slot.id] = slot.time;
+                        slotMap[slot.slotId] = slot.startTime;
                     }
                 );
                 setSlotMap(slotMap);
             })
-            .catch((error) => console.error('Error fetching data:', error));
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
     }, []);
 
     return (
-        <GlobalContext.Provider value={{districtMap, tableTypeMap, slotMap}}>
+        <GlobalContext.Provider value={{districtMap, tableTypeMap, slotMap, bookingStatusMap}}>
             {children}
         </GlobalContext.Provider>
     );

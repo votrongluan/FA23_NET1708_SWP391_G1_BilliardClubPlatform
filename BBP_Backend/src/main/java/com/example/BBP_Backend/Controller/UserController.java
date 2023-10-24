ff83d261d4b60871cb9288abad56e6b92e3bf94d
SimondaVinciii <hqthinh154@gmail.com>
2023-10-24 14:15:17 +0700

    Update api

--- BBP_Backend/src/main/java/com/example/BBP_Backend/Controller/UserController.java
+++ BBP_Backend/src/main/java/com/example/BBP_Backend/Controller/UserController.java
@@ -1,6 +1,7 @@
 package com.example.BBP_Backend.Controller;
 
 import com.example.BBP_Backend.Model.User;
+import com.example.BBP_Backend.Request.UpdatePasswordRequest;
 import com.example.BBP_Backend.Request.UserUpdateRequest;
 import com.example.BBP_Backend.Response.ResponseObject;
 import com.example.BBP_Backend.Service.UserService;
@@ -9,11 +10,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin("http://localhost:5173")
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
-
 
 
     @PutMapping("/updateUser/{userId}")
@@ -24,13 +25,46 @@ public class UserController {
         User updatedUserInfo = userService.updateUserInformation(userId, updatedUser);
         if (updatedUserInfo != null) {
             return ResponseEntity.ok(
-                    new ResponseObject("Ok", "Update information successfully",updatedUserInfo));
+                    new ResponseObject("Ok", "Update information successfully", updatedUserInfo));
         } else {
             return ResponseEntity.badRequest().body(
                     new ResponseObject("Failed", "Can not find !!!", "")
             );
         }
     }
+
+    @PutMapping("/updatePassword/{userId}")
+    public ResponseEntity<ResponseObject> updatePassword(@PathVariable(name = "userId") int userId,
+                                                         @RequestBody UpdatePasswordRequest update
+    ) {
+        String status = userService.updateUserPassword(userId, update);
+        if (status.equals("Succesful")) {
+            return ResponseEntity.ok(new ResponseObject(
+                    "Ok", "Update password successfully!", ""
+            ));
+        }
+//        if (status.equals("Failed")) {
+//            return ResponseEntity.badRequest().body(new ResponseObject(
+//                    "Failed", "Can not update password!!!", "")
+//            );
+//        }
+         if(status.equals("samePassword")) {
+            return ResponseEntity.badRequest().body(new ResponseObject(
+                    "Failed", "Same password!!!", "")
+            );
+        }
+         return  ResponseEntity.badRequest().body(new ResponseObject(
+                    "Failed", "Can not find User!!!", ""));
+    }
+}
+
+
+
+
+
+
+
+
 //    @PutMapping("/updateUser2/{userId}")
 //    public ResponseEntity<User> updateUserInformation2(
 //            @PathVariable("userId") int userId,
@@ -43,4 +77,4 @@ public class UserController {
 //            return ResponseEntity.notFound().build();
 //        }
 //    }
-}
+
---
+++ BBP_Backend/src/main/java/com/example/BBP_Backend/Response/StaffInfoResponse.java
@@ -0,0 +1,17 @@
+package com.example.BBP_Backend.Response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class StaffInfoResponse {
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String phone;
+}

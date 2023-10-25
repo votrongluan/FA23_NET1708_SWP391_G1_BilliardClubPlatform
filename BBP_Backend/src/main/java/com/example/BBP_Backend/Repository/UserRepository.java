1d74d41c2acd39ae6ad02811ea4f95a3e55274ff
votrongluan <trongluan115@gmail.com>
2023-10-25 17:10:06 +0700

    fix: add more property in login api

--- BBP_Backend/src/main/java/com/example/BBP_Backend/Repository/UserRepository.java
+++ BBP_Backend/src/main/java/com/example/BBP_Backend/Repository/UserRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -15,6 +16,4 @@ public interface UserRepository extends JpaRepository<User,Integer> {
     User getUserByUserId(int userId);
 
     User getUserByUsername(String username);
-
-    List<User> getUserByRole()
 }

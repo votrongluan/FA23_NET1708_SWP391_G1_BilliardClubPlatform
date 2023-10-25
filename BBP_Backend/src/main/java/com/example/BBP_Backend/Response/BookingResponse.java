b43b6ffa0f0dfde034e241c90bad4b13548eeef2
Chph21 <damchanphong@gmail.com>
2023-10-25 14:47:06 +0700

    fix: date format

--- BBP_Backend/src/main/java/com/example/BBP_Backend/Response/BookingResponse.java
+++ BBP_Backend/src/main/java/com/example/BBP_Backend/Response/BookingResponse.java
@@ -1,5 +1,6 @@
 package com.example.BBP_Backend.Response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,6 +18,7 @@ public class BookingResponse {
     private String clubName;
     private String clubAddress;
     private int districtId;
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private Date date;
     private double star;
     private String comment;

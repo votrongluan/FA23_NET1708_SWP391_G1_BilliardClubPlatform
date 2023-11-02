e93277f8694be830a7457ea7264be5720dd4b3dc
huydq911 <chimsedihia@gmail.com>
2023-11-02 16:12:51 +0700

    fix: some UI bug

--- BBP_Frontend/src/components/Club.jsx
+++ BBP_Frontend/src/components/Club.jsx
@@ -1,42 +1,42 @@
-import {Button, Card, CardBody, CardFooter, CardHeader, Divider, Image, Text} from "@chakra-ui/react";
-import {CalendarIcon, StarIcon, ViewIcon} from "@chakra-ui/icons";
-import {GlobalContext} from "../context/GlobalContext.jsx";
-import {useContext} from "react";
-import {Link} from "react-router-dom";
+import { Button, Card, CardBody, CardFooter, CardHeader, Divider, Image, Text } from "@chakra-ui/react";
+import { CalendarIcon, StarIcon, ViewIcon } from "@chakra-ui/icons";
+import { GlobalContext } from "../context/GlobalContext.jsx";
+import { useContext } from "react";
+import { Link } from "react-router-dom";
 
-function Club({data}) {
+function Club({ data }) {
     const club = data?.club;
     club.noRating = data?.noRating;
     club.noBooking = data?.noBooking;
     club.rating = data?.rating;
 
-    const {districtMap} = useContext(GlobalContext);
+    const { districtMap } = useContext(GlobalContext);
 
     return (
         <Card key={club.clubId} borderTop="4px" borderColor="blue.500" bg="white">
             <CardHeader pb={0} color="gray.700">
-                <Image src={club?.avatarLink} height="200px" width="100%" objectFit="cover"/>
+                <Image src={club?.avatarLink} height="200px" width="100%" objectFit="cover" />
             </CardHeader>
 
             <CardBody color="gray.500">
                 <Text color="black" fontSize="20px">{club?.clubName}</Text>
-                <Text mt={2}>{club?.address}, {districtMap[club.districtId]}</Text>
+                <Text height="50px" mt={2}>{club?.address}, {districtMap[club.districtId]}</Text>
                 {Boolean(club?.noRating) ?
                     <Text mt={4} gap={2} display="flex" alignItems="center" justifyContent="flex-end" color="gray.500">
-                        <StarIcon color="yellow.500"/>{club.rating} ({club.noRating} đánh giá)
+                        <StarIcon color="yellow.500" />{club.rating} ({club.noRating} đánh giá)
                     </Text> :
                     <Text mt={4} gap={2} display="flex" alignItems="center" justifyContent="flex-start"
-                          color="gray.500">
-                        <StarIcon color="yellow.500"/>Chưa có đánh giá
+                        color="gray.500">
+                        <StarIcon color="yellow.500" />Chưa có đánh giá
                     </Text>}
             </CardBody>
 
-            <Divider borderColor="gray.300"/>
+            <Divider borderColor="gray.300" />
 
             <CardFooter display="flex" alignItems="center" justifyContent="space-between">
-                <Link to={`/clubs/${club.clubId}`}><Button variant="ghost" leftIcon={<ViewIcon/>}>Chi
+                <Link to={`/clubs/${club.clubId}`}><Button variant="ghost" leftIcon={<ViewIcon />}>Chi
                     tiết</Button></Link>
-                <Link to={`/book/${club.clubId}`}><Button colorScheme="yellow" leftIcon={<CalendarIcon/>}>Đặt
+                <Link to={`/book/${club.clubId}`}><Button colorScheme="yellow" leftIcon={<CalendarIcon />}>Đặt
                     bàn</Button></Link>
             </CardFooter>
         </Card>
